07f41609f779f5ba6b008b075a6451d0079ce328
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-20 15:31:33 +0900

    罫線が四辺の中央から伸びる形態をlinear-gradientを使って実装するコード
    罫線が元からあるものとホバーで現れるもので1pxずれる。
    このズレが治せないか。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,9 +1,9 @@
 // 見出し2に付いてる属性『letter-spacing: .5em;』を最後の文字だけ取り去る
 let headingTwo = document.querySelectorAll("h2")
 for (let heading of headingTwo) {
-  let orgText = heading.innerHTML
-  let lastChar = orgText.slice(-1)
-  heading.innerHTML = orgText.replace(lastChar, `<span class="remove-letter-spacing">${lastChar}</span>`)
+  let lastChar = heading.textContent.slice(-1)
+  let preText = heading.textContent.slice(0, -1)
+  heading.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
 }
 
 let pictures = document.querySelectorAll(".wrapper picture")
